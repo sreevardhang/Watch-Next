5e474970bbe982c8d21e9dcaba65b19463944ee2
Sreevardhan Gullipalli <sreevardhang@MacBook-Air-von-Sreevardhan.local>
2026-08-04 17:40:09 +0530

    Cleaned code. Basic similarity based recommendation works

--- main.py
+++ main.py
@@ -30,12 +30,10 @@ def find_title(medialist, usertitle):
 def calculate_score(selectedtitle, medialist):
     scores = {}
 
-    medialist = (item for item in medialist if selectedtitle['title'] not in item['title'])
-
     selected_genres = set(genre.strip().lower() for genre in selectedtitle['genres'].split(','))
 
     for item in medialist:
-        if selectedtitle['title'] in item['title']:
+        if selectedtitle['title'] == item['title']:
             continue
 
         item_score = 0
@@ -56,7 +54,7 @@ def calculate_score(selectedtitle, medialist):
 
     return scores
 
-def get_recommendations(scores, limit = 3):
+def get_recommendations(scores, limit=3):
     positive_scores = {title:score for title,score in scores.items() if score > 0}
 
     sorted_scores = sorted(positive_scores.items(), key=lambda pair: pair[1], reverse = True)
@@ -71,46 +69,13 @@ def main():
 
     if selected_title is None:
         print("Title not found")
-    else:
-        scores = calculate_score(selected_title, medialib)
-
+        return
+    
+    scores = calculate_score(selected_title, medialib)
     recommendations = get_recommendations(scores)
 
     for title, score in recommendations:
         print(f"{title} -- Similarity Score: {score}")
-
-    # print(f"Available moods: {moods}")
-    # print("Type 'exit' or 'quit' at any time to quit the program")
-
-    # while True:
-    #     mood_input = input("What mood are you in? ").strip().lower()
-    #     if mood_input in ['exit','quit']:
-    #         break
-    #     if mood_input not in moods:
-    #         print("Choose one of the available moods ")
-    #         continue
-    #     type_input = input("Choose: 'movie' or 'show'? ").strip().lower()
-    #     if type_input in ['exit','quit']:
-    #         break
-    #     if type_input not in ['movie', 'show']:
-    #         print("Enter exactly 'movie' or 'show' ")
-    #         continue
-    #     genre_input = input("Enter a preferred genre, or press Enter to skip ").strip().lower()
-    #     if genre_input in ['exit', 'quit']:
-    #         break
-
-    #     matches = find_recommendations(medialib, mood_input, type_input, genre_input)
-
-    #     if matches:
-    #         print("\nRecommendations:\n")
-
-    #         for item in matches:
-    #             print(f"Title: {item['title']}")
-    #             print(f"Genres: {item['genres']}")
-    #             print(f"Runtime: {item['runtime']}")
-    #             print("-" * 40)
-    #     else:
-    #         print("Unfortunately a match was not found :(")
 
 if __name__ == "__main__":
     main()
